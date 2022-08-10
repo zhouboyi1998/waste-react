f7f9b7f35b5b863b366403253be14a644bb09528
zhouboyi <1144188685@qq.com>
2022-08-10 23:32:46 +0800

    添加home页面，配置.ts文件中使用style

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './home.module.scss'
+
+function Home() {
+    return (
+        <div>
+            <h1 className={ style.header }>垃圾分类查询</h1>
+        </div>
+    )
+}
+
+export default Home
