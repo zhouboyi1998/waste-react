5c928b292495ee85ab1f4da60b1f9301d4f83f51
zhouboyi <1144188685@qq.com>
2023-10-08 10:48:20 +0800

    feat(route): 添加路由功能，点击菜单切换页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Index from './pages/Index'
+import LayoutView from './views/Layout'
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <Index/> }/>
-            </Routes>
-        </BrowserRouter>
+        <LayoutView/>
     )
 }
 
